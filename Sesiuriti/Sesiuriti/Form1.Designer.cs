63bdc052b4d2036ce46caee97436a63c07afdbd8
Misanea777 <56073861+Misanea777@users.noreply.github.com>
2020-09-08 14:52:48 +0300

    Add files via upload

---
+++ Sesiuriti/Sesiuriti/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace Sesiuriti
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.AddButton = new System.Windows.Forms.Button();
+            this.OpenButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.AddNewButton = new System.Windows.Forms.Button();
+            this.CancelButton = new System.Windows.Forms.Button();
+            this.LoadingLabel = new System.Windows.Forms.Label();
+            this.PoliciesTreeView = new System.Windows.Forms.TreeView();
+            this.FileNameTextBox = new System.Windows.Forms.TextBox();
+            this.FileNameLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // AddButton
+            // 
+            this.AddButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.AddButton.Location = new System.Drawing.Point(1158, 408);
+            this.AddButton.Name = "AddButton";
+            this.AddButton.Size = new System.Drawing.Size(86, 31);
+            this.AddButton.TabIndex = 3;
+            this.AddButton.Text = "Add";
+            this.AddButton.UseVisualStyleBackColor = true;
+            this.AddButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // OpenButton
+            // 
+            this.OpenButton.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.OpenButton.Location = new System.Drawing.Point(293, 408);
+            this.OpenButton.Name = "OpenButton";
+            this.OpenButton.Size = new System.Drawing.Size(105, 31);
+            this.OpenButton.TabIndex = 4;
+            this.OpenButton.Text = "Open";
+            this.OpenButton.UseVisualStyleBackColor = true;
+            this.OpenButton.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.textBox1.Location = new System.Drawing.Point(417, 412);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(468, 22);
+            this.textBox1.TabIndex = 5;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.AllowDrop = true;
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(412, 24);
+            this.comboBox1.TabIndex = 6;
+            this.comboBox1.SelectionChangeCommitted += new System.EventHandler(this.comboBox1_SelectionChangeCommitted);
+            // 
+            // AddNewButton
+            // 
+            this.AddNewButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.AddNewButton.Location = new System.Drawing.Point(1102, 410);
+            this.AddNewButton.Name = "AddNewButton";
+            this.AddNewButton.Size = new System.Drawing.Size(96, 29);
+            this.AddNewButton.TabIndex = 7;
+            this.AddNewButton.Text = "Add new  policy";
+            this.AddNewButton.UseVisualStyleBackColor = true;
+            this.AddNewButton.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // CancelButton
+            // 
+            this.CancelButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.CancelButton.Location = new System.Drawing.Point(1012, 409);
+            this.CancelButton.Name = "CancelButton";
+            this.CancelButton.Size = new System.Drawing.Size(84, 31);
+            this.CancelButton.TabIndex = 8;
+            this.CancelButton.Text = "Cancel";
+            this.CancelButton.UseVisualStyleBackColor = true;
+            this.CancelButton.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // LoadingLabel
+            // 
+            this.LoadingLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.LoadingLabel.AutoSize = true;
+            this.LoadingLabel.Location = new System.Drawing.Point(596, 183);
+            this.LoadingLabel.Name = "LoadingLabel";
+            this.LoadingLabel.Size = new System.Drawing.Size(71, 17);
+            this.LoadingLabel.TabIndex = 10;
+            this.LoadingLabel.Text = "Loading...";
+            // 
+            // PoliciesTreeView
+            // 
+            this.PoliciesTreeView.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.PoliciesTreeView.Location = new System.Drawing.Point(25, 42);
+            this.PoliciesTreeView.Name = "PoliciesTreeView";
+            this.PoliciesTreeView.Size = new System.Drawing.Size(486, 360);
+            this.PoliciesTreeView.TabIndex = 11;
+            // 
+            // FileNameTextBox
+            // 
+            this.FileNameTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.FileNameTextBox.Location = new System.Drawing.Point(1012, 380);
+            this.FileNameTextBox.Name = "FileNameTextBox";
+            this.FileNameTextBox.Size = new System.Drawing.Size(186, 22);
+            this.FileNameTextBox.TabIndex = 12;
+            // 
+            // FileNameLabel
+            // 
+            this.FileNameLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.FileNameLabel.AutoSize = true;
+            this.FileNameLabel.Location = new System.Drawing.Point(939, 383);
+            this.FileNameLabel.Name = "FileNameLabel";
+            this.FileNameLabel.Size = new System.Drawing.Size(67, 17);
+            this.FileNameLabel.TabIndex = 13;
+            this.FileNameLabel.Text = "FileName";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1266, 469);
+            this.Controls.Add(this.FileNameLabel);
+            this.Controls.Add(this.FileNameTextBox);
+            this.Controls.Add(this.PoliciesTreeView);
+            this.Controls.Add(this.LoadingLabel);
+            this.Controls.Add(this.CancelButton);
+            this.Controls.Add(this.AddNewButton);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.OpenButton);
+            this.Controls.Add(this.AddButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button AddButton;
+        private System.Windows.Forms.Button OpenButton;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button AddNewButton;
+        private System.Windows.Forms.Button CancelButton;
+        private System.Windows.Forms.Label LoadingLabel;
+        private System.Windows.Forms.TreeView PoliciesTreeView;
+        private System.Windows.Forms.TextBox FileNameTextBox;
+        private System.Windows.Forms.Label FileNameLabel;
+    }
+}
+
---
+++ Sesiuriti/Sesiuriti/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace Sesiuriti
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Controller();
+        }
+
+
+
+        private void Controller()
+        {
+            OpenButton.Visible = false;
+            textBox1.Visible = false;
+            AddButton.Visible = false;
+            CancelButton.Visible = false;
+            LoadingLabel.Visible = false;
+            FileNameLabel.Visible = false;
+            FileNameTextBox.Visible = false;
+
+            PoliciesTreeView.CheckBoxes = true;
+
+            string[] paresedFilesDir = getParsedFilesDir();
+            updatePoliciesList(paresedFilesDir);
+
+        }
+
+        private void updatePoliciesList(string[] policiesList)
+        {
+            comboBox1.Items.Clear();
+            foreach (string s in policiesList)
+            {
+                comboBox1.Items.Add(Path.GetFileName(s));
+            }
+        }
+
+        private string[] getParsedFilesDir()
+        {
+            var path = System.IO.Path.GetDirectoryName(
+                System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase).Substring(6);
+            return Directory.GetFiles(path, "*.xml");
+        }
+
+
+
+
+        public OpenFileDialog ofd = new OpenFileDialog();
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ofd.Filter = "AUDIT|*.audit";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = ofd.FileName;
+                FileNameTextBox.Text = (Path.GetFileNameWithoutExtension(ofd.SafeFileName));
+
+
+            }
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.TextLength != 0)
+            {
+                OpenButton.Visible = false;
+                textBox1.Visible = false;
+                AddButton.Visible = false;
+                CancelButton.Visible = false;
+                FileNameLabel.Visible = false;
+                FileNameTextBox.Visible = false;
+
+                Cursor = Cursors.WaitCursor;
+                
+
+                AuditFileParser parser = new AuditFileParser(textBox1.Text, FileNameTextBox.Text);
+                parser.fileParser();
+                updatePoliciesList(getParsedFilesDir());
+
+                Cursor = Cursors.Arrow;
+                AddNewButton.Visible = true;
+            }
+        }
+
+
+        private void comboBox1_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            PoliciesTreeView.Nodes.Clear();
+            string name = comboBox1.SelectedItem.ToString();
+            name = name.Substring(0, name.Length - 4);
+            TreeNode mainNode =  PoliciesTreeView.Nodes.Add(name);
+            XML_Traverser xmlTraverser = new XML_Traverser(comboBox1.SelectedItem.ToString());
+            xmlTraverser.AddNode(xmlTraverser.root, mainNode);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            AddNewButton.Visible = false;
+            OpenButton.Visible = true;
+            textBox1.Visible = true;
+            AddButton.Visible = true;
+            CancelButton.Visible = true;
+            FileNameLabel.Visible = true;
+            FileNameTextBox.Visible = true;
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            OpenButton.Visible = false;
+            textBox1.Visible = false;
+            CancelButton.Visible = false;
+            AddButton.Visible = false;
+            AddNewButton.Visible = true;
+            FileNameLabel.Visible = false;
+            FileNameTextBox.Visible = false;
+        }
+    }
+}
